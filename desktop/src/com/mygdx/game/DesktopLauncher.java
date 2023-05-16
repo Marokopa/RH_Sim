9735226786f94ac7168b04880054cddfad72a30f
Marokopa <velikayadvoica@yandex.ru>
2023-05-16 19:09:05 +0300

    2

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -1,5 +1,8 @@
 package com.mygdx.game;
 
+import static com.mygdx.game.MyGdxGame.SCR_HEIGHT;
+import static com.mygdx.game.MyGdxGame.SCR_WIDTH;
+
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
@@ -8,6 +11,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
+		config.setWindowedMode((int) SCR_WIDTH, (int) SCR_HEIGHT);
 		config.setTitle("Robin Hood Simulator");
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
